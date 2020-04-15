d9138f9db7f83090532024f54beaa707d53a1366
BlainJul <blain.julien@gmail.com>
2020-04-15 10:04:50 +0200

    creation d'un dynamic web project, conversion en maven, add dependencies

---
+++ S10L47DAO/src/org/easystudy/spring/AppController.java
@@ -0,0 +1,16 @@
+package org.easystudy.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AppController {
+
+		@RequestMapping("/")
+		public ModelAndView homepage() {
+			ModelAndView model = new ModelAndView("index");
+			return model;
+		}
+	
+}
