0c7d188f159193ac43f421eca93f2b91b5a51ec1
BlainJul <blain.julien@gmail.com>
2020-04-21 07:43:12 +0200

    creation du bean de connection par annotation

--- S11L52DAOAnnotation/src/org/studyeasy/spring/AppController.java
+++ S11L52DAOAnnotation/src/org/studyeasy/spring/AppController.java
@@ -21,24 +21,6 @@ import org.studyeasy.spring.model.User;
 @Controller
 public class AppController {
 	
-	
-	@PostMapping("/displayUserInfo")
-	public ModelAndView displayUserInfo(@ModelAttribute("user") @Valid User user, BindingResult result) { //modelAttribute du form qui donne les caract des champs si error
-		ModelAndView modelAndView = new ModelAndView("displayUserInfo");
-		modelAndView.addObject("user", user);
-		
-		// annotation valid et bindingResult permettent de valider les données par rapport aux info données par le model
-		if(result.hasErrors()) {	
-			ModelAndView repopulatingModelAndView = new ModelAndView("userFormView");
-			repopulatingModelAndView.addObject("user", user);
-			return  repopulatingModelAndView;
-		}
-		else {
-			return modelAndView;
-		}
-		
-	}
-	
 	@RequestMapping("/")
 	public ModelAndView userList() {
 		ModelAndView model = new ModelAndView("userList");
@@ -53,34 +35,5 @@ public class AppController {
 		context.close();
 		return model;
 	}
-	
-	/*
-	@GetMapping("/index")
-	public ModelAndView home(){
-		ModelAndView modelAndView = new ModelAndView("userFormView");
-		User user = new User();
-		modelAndView.addObject("user", user); //permet de lier le model au formulaire de la vue
-		return modelAndView;
-		
-		//Renvoie la vue d'une autre facon
-		  User user = new User();
-		
-		Map<String, String> genderMap = new HashMap<String, String>();
-		genderMap.put("male", "Male");
-		genderMap.put("female", "Female");
-		
-		map.addAttribute("genderMap", genderMap); //permet de passer les attributs dans la vue avec radiobuttonS 
-		
-		Map<String,String> countryMap = new HashMap<String, String>();
-		countryMap.put("India", "Inde");
-		countryMap.put("France", "France");
-		map.addAttribute("countryMap", countryMap);
-		
-		map.addAttribute("user", user); //permet de lier le model au formulaire de la vue
-		return "userFormView";
-		 
-		
-	}* */
-
 	
 }
