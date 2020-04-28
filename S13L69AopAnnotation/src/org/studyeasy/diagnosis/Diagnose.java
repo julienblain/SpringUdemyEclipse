7a56b4254826fef7bb008e8def8348d5c29edd7e
BlainJul <blain.julien@gmail.com>
2020-04-28 08:33:29 +0200

    annotation advice arround avant et apres methode

--- S13L69AopAnnotation/src/org/studyeasy/diagnosis/Diagnose.java
+++ S13L69AopAnnotation/src/org/studyeasy/diagnosis/Diagnose.java
@@ -1,8 +1,10 @@
 package org.studyeasy.diagnosis;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -15,10 +17,11 @@ public class Diagnose {
 		
 	}
 	
+	/*
 	@Before("run()") //on peut mettre execution blabla ou le pointcut
 	public void beforeAdvice() {
 		System.out.println("before advice");
-	}
+	}*/
 	
 	/*
 	@After("run()") // s'execute si exception
@@ -29,11 +32,26 @@ public class Diagnose {
 	@AfterReturning("run()") //ne s'execute pas si exception
 	public void afterAdvice() {
 		System.out.println("After advice ");
-	}*/
+	}
 	
 	@AfterThrowing("run()") //s'execute seulement si une exception est levée
 	public void afterAdvice() {
 		System.out.println("After advice ");
+	}
+	*/
+	
+	@Around("run()") //avant et apres run si jp.proceed sinon nop
+	public void aroundAdvice(ProceedingJoinPoint jp) {
+		System.out.println("Around advice initial msg ");
+		
+		try {
+			jp.proceed();
+		} catch (Throwable e) {
+			
+			e.printStackTrace();
+		}
+		
+		System.out.println("Around advice later msg ");
 	}
 	
 }
