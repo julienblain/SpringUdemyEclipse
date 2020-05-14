11f0df81917fa82d7b35b52585720221f5c66f1f
BlainJul <blain.julien@gmail.com>
2020-05-15 00:43:00 +0200

    connection du logueur à la db

--- S17SpringSecurityWithAnnotation/src/org/studyeasy/spring/config/AppConfig.java
+++ S17SpringSecurityWithAnnotation/src/org/studyeasy/spring/config/AppConfig.java
@@ -1,10 +1,24 @@
 package org.studyeasy.spring.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class AppConfig {
 
-	
+	@Bean(name="dataSource")
+	public DataSource getDataSource() {
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/project1?useSSL=false"); //evite d avoir l'erreur ssl dans la console 
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		
+		return dataSource;
+	}
 	
 }
--- S17SpringSecurityWithAnnotation/src/org/studyeasy/spring/config/SecurityConfig.java
+++ S17SpringSecurityWithAnnotation/src/org/studyeasy/spring/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.studyeasy.spring.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,11 +13,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws
-	Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("user").password("123456").roles("USER");
+	DataSource dataSource;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
 	}
 	
 	@Override
